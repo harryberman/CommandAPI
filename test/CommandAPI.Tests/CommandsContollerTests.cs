8429ef6bfbcb4b700447243e40ed12d109b4cfd3
Harry Berman <harry@dynamicsystemsconcepts.com>
2020-10-15 02:46:36 -0500

    fixed broken test

--- test/CommandAPI.Tests/CommandsContollerTests.cs
+++ test/CommandAPI.Tests/CommandsContollerTests.cs
@@ -225,7 +225,7 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             // Assert
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NotFoundResult>(result);
         }
 
         private List<Command> GetCommands(int num)
